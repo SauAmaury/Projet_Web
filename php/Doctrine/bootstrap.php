4ae38beb9db5637a2123ef618b269e1b0a485a66
Amaury Sauvage <diabolo52000@hotmail.fr>
2018-10-28 23:04:40 +0100

    Mise en place de Doctrine

---
+++ php/Doctrine/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "../vendor/autoload.php";
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+
+$conn = array(
+    'dbname' => 'mydb',
+    'user' => 'user',
+    'password' => 'simple',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+
+$entityManager = EntityManager::create($conn, $config);
